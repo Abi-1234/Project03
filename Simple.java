2c7eb911c3d698ccb4cf940217f8804f97d04046
Abi-1234 <abinayakannan1999@gmail.com>
2021-12-29 11:56:10 +0530

    task

---
+++ Simple.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Simple {
+	public void saving()
+	 {
+		 System.out.println("saving is 4%");
+	 }
+	 public static void main(String[] args) {
+		Simple h=new Simple();
+		h.saving();
+		}
+}
